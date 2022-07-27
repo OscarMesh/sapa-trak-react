237b8eea54d604777ccb62422cb07e1b3b288764
Oscar Mesh <okerekeifeanyi65@gmail.com>
2022-07-27 15:57:44 +0100

    first commit

---
+++ src/component/Balance.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Balance = () => {
+  return (
+    <div className="balance">
+      <p>Available Balance</p>
+      <div className="amt">
+        <p id="balance">#30,000</p>
+      </div>
+      </div>
+  )
+}
+
+export default Balance;
---
+++ src/component/IncomeExpences.js
@@ -0,0 +1,23 @@
+import React from 'react'; 
+import rectangle from '../rectangle.svg';
+export const IncomeExpences = () => {
+  return (
+    <div className="inc-exp">
+      <div className="inc">
+        <p>Income</p>
+        <div class="amt">
+          <span id="income">#30,000</span>
+        </div>
+      </div>
+      <img src={rectangle} alt="" />
+      <div className="exp">
+        <p>Expenses</p>
+        <div className="amt">
+          <p id="expenses">#30,000</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default IncomeExpences;
